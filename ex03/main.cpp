3ca4d1d96f61498115d585aea4693d03814b634a
xvanko <445310@mail.muni.cz>
2018-10-10 17:39:57 +0200

    add benchmarks for random sequence access for vector and list

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -9,47 +9,49 @@ using std::vector;
 
 const int ACCESSED_ITEMS_COUNT = 5;
 
-void benchmark_vector_10_access_random_sequence(void* data)
+void benchmark_vector_access_random_sequence(void* data)
 {
+    std::pair<int, int>& settings = *(std::pair<int, int>*)data;
+    const int size = settings.first;
+    const int randomPosition = settings.second;
+
     int sum = 0;
-    vector<int> v(10, 2);
+    vector<int> v(size, 0);
+    Benchmarking::size_info(std::to_string(size));
     Benchmarking::start();
-    for (int i = v.size() / 2; i < v.size(); i++) {
+    for (int i = randomPosition; i < randomPosition + ACCESSED_ITEMS_COUNT && i < v.size(); i++) {
         sum += v[i];
     }
     Benchmarking::stop();
     sum ^= sum;
 }
 
-void benchmark_vector_100_access_random_sequence(void* data)
+void benchmark_list_access_random_sequence(void* data)
 {
+    std::pair<int, int>& settings = *(std::pair<int, int>*)data;
+    const int size = settings.first;
+    const int randomPosition = settings.second;
+
     int sum = 0;
-    vector<int> v(100, 2);
+    std::list<int> list(size, 0);
+    const auto sequenceEnd = std::next(list.begin(), randomPosition + ACCESSED_ITEMS_COUNT);
+    Benchmarking::size_info(std::to_string(size));
     Benchmarking::start();
-    for (int i = v.size() / 2; i < v.size(); i++) {
-        sum += v[i];
+    for (auto it = std::next(list.begin(), randomPosition); it != list.end() && it != sequenceEnd; it++) {
+        sum += *it;
     }
     Benchmarking::stop();
     sum ^= sum;
 }
-
-void benchmark_vector_10000_access_random_sequence(void* data)
-{
-    int sum = 0;
-    vector<int> v(10000, 2);
-    Benchmarking::start();
-    for (int i = v.size() / 2; i < v.size(); i++) {
-        sum += v[i];
-    }
-    Benchmarking::stop();
-    sum ^= sum;
-}
-
 
 int main(int argc, char** argv)
 {
     Benchmarking::init(argc, argv);
-    //BENCHMARKING_RUN(benchmark_vector_10_access_random_sequence, NULL);
+    for (int vectorSize = 10; vectorSize <= 1000000; vectorSize *= 10) {
+        std::pair<int, int> settings{vectorSize, rand() % vectorSize};
+        BENCHMARKING_RUN(benchmark_vector_access_random_sequence, &settings);
+        BENCHMARKING_RUN(benchmark_list_access_random_sequence, &settings);
+    }
 
     return 0;
 }
