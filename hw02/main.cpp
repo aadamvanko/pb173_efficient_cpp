8fb58b131c6ec1465dffd12568047c83fa469008
adam <445310@mail.muni.cz>
2018-10-14 10:50:37 +0200

    hw02 ready for implementing cache optimized variant

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <random>
 
 #include "../hw01/benchmark_tool.hpp"
 #include "matrix2d.hpp"
 
+struct BenchmarkData
+{
+    Matrix2D matrixA;
+    Matrix2D matrixB;
+};
+
+Matrix2D generateRandomMatrix(std::mt19937& generator, int size)
+{
+    Matrix2D matrix(size, size);
+    std::uniform_int_distribution<> dist(0, 1);
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrix[i][j] = dist(generator);
+        }
+    }
+
+    return matrix;
+}
+
 void naive(void* data)
 {
-    const int size = *(int*)data;
-    Matrix2D matrixA(size, size);
-    Matrix2D matrixB(size, size);
-    Benchmarking::size_info("size=" + std::to_string(size));
+    const BenchmarkData& matrices = *(BenchmarkData*)data;
+
+    Benchmarking::size_info("size=" + std::to_string(matrices.matrixA.size()));
     Benchmarking::start();
-    Matrix2D result = matrixA.naiveMultiplication(matrixB);
+    Matrix2D result = matrices.matrixA.naiveMultiplication(matrices.matrixB);
     Benchmarking::stop();
     auto s = result.size();
     s ^= s;
@@ -18,12 +40,11 @@ void naive(void* data)
 
 void cache_eff(void* data)
 {
-    const int size = *(int*)data;
-    Matrix2D matrixA(size, size);
-    Matrix2D matrixB(size, size);
-    Benchmarking::size_info("size=" + std::to_string(size));
+    const BenchmarkData& matrices = *(BenchmarkData*)data;
+
+    Benchmarking::size_info("size=" + std::to_string(matrices.matrixA.size()));
     Benchmarking::start();
-    Matrix2D result = matrixA.cacheEfficientMultiplication(matrixB);
+    Matrix2D result = matrices.matrixA.cacheEfficientMultiplication(matrices.matrixB);
     Benchmarking::stop();
     auto s = result.size();
     s ^= s;
@@ -32,10 +53,30 @@ void cache_eff(void* data)
 int main(int argc, char** argv)
 {
     Benchmarking::init(argc, argv);
-    for (int size = 10; size <= 1000; size *= 10)
+
+    std::mt19937 generator;
+    generator.seed(1);
+    std::uniform_int_distribution<> dist(0, 1);
+
+    BenchmarkData benchmarkData;
+    for (int size = 8; size <= 128; size *= 2)
     {
-        BENCHMARKING_RUN(naive, &size);
-        BENCHMARKING_RUN(cache_eff, &size);
+        benchmarkData.matrixA = generateRandomMatrix(generator, size);
+        benchmarkData.matrixB = generateRandomMatrix(generator, size);
+
+        // benchmarkData.matrixA.print(std::cout);
+        // std::cout << std::endl;
+        // benchmarkData.matrixB.print(std::cout);
+
+        if (benchmarkData.matrixA.naiveMultiplication(benchmarkData.matrixB) !=
+            benchmarkData.matrixA.cacheEfficientMultiplication(benchmarkData.matrixB))
+        {
+            std::cout << "ERROR IN MULTIPLICATION!" << std::endl;
+        }
+
+        BENCHMARKING_RUN(naive, &benchmarkData);
+        BENCHMARKING_RUN(cache_eff, &benchmarkData);
     }
+
     return 0;
 }
--- hw02/matrix2d.hpp
+++ hw02/matrix2d.hpp
@@ -12,6 +12,8 @@ private:
     std::vector<std::vector<RealType>> m_data;
 
 public:
+    Matrix2D() = default;
+
     Matrix2D(int rows, int cols) :
         m_data(std::vector<std::vector<RealType>>(rows, std::vector<RealType>(cols, 0)))
     {
@@ -22,6 +24,14 @@ public:
     }
 
     size_t size() const { return m_data.size(); }
+
+    bool operator==(const Matrix2D& rhs){ return m_data == rhs.m_data; }
+    bool operator!=(const Matrix2D& rhs){ return !(*this == rhs); }
+
+    std::vector<RealType>& operator[](int row)
+    {
+        return m_data[row];
+    }
 
     void print(std::ostream& os)
     {
@@ -36,7 +46,7 @@ public:
         }
     }
 
-    Matrix2D naiveMultiplication(const Matrix2D& other)
+    Matrix2D naiveMultiplication(const Matrix2D& other) const
     {
         Matrix2D result(m_data.size(), other.m_data[0].size());
         size_t size = m_data.size();
@@ -53,7 +63,7 @@ public:
         return result;
     }
 
-    Matrix2D cacheEfficientMultiplication(const Matrix2D& other)
+    Matrix2D cacheEfficientMultiplication(const Matrix2D& other) const
     {
         Matrix2D result(m_data.size(), other.m_data[0].size());
         size_t size = m_data.size();
@@ -61,10 +71,12 @@ public:
         {
             for (int j = 0; j < size; j++)
             {
+                RealType sum = 0;
                 for (int k = 0; k < size; k++)
                 {
-                    result.m_data[i][j] += m_data[i][k] * other.m_data[k][i];
+                    sum += m_data[i][k] * other.m_data[k][i];
                 }
+                result.m_data[i][j] = sum;
             }
         }
 
